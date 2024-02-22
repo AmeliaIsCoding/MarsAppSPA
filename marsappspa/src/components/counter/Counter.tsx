e22e62488adda35c82b00034b2f738325491199d
AmeliaIsCoding <Amelia.Blackwell@softwire.com>
2024-02-22 14:03:23 +0000

    Part 2.3 and 2.4

---
+++ marsappspa/src/components/counter/Counter.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Counter.css';
+
+function Counter() {
+  const [count, setCount] = React.useState(
+        parseInt(localStorage.getItem("count") ?? "0")
+  );
+  React.useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count])
+  return (
+    <div className="Counter">
+        <button className="Counter-button" onClick={() => setCount(count+1)}>
+          {count}
+        </button>
+    </div>
+  );
+}
+
+export default Counter;
